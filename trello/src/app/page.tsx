965b8f5d61b62c77e752b5226430695bf0f76056
Imalsha-karunarathna <laksarani@liveroom.dev>
2025-05-03 10:25:30 +0530

    change color

--- trello/src/app/page.tsx
+++ trello/src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="container mx-auto py-8 px-4">
-      <h1 className="text-3xl font-bold mb-8 text-blue-950">
+      <h1 className="text-3xl font-bold mb-8 text-red-950">
         Task Management App
       </h1>
       <TaskBoard />
